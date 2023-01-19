3e6c77a1c0fc76594519c797cbf24bbbf341b3d2
hyunjiyoo <hjyoo.dev@gmail.com>
2023-01-19 16:36:14 +0900

    Header styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from "components/Header/Header";
+import Products from "pages/Products";
 import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
+    <div className='m-auto w-9/12'>
       <Header />
+      <Products />
       <Outlet />
-    </>
+    </div>
   );
 };
 
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import { BsGiftFill, BsCart4 } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
+
   return (
-    <div>
-      Header
-    </div>
+    <header className='flex items-center justify-between'>
+      <Link to="/" className='flex items-center text-orange-600 cursor-pointer'>
+        <BsGiftFill className='text-xl'/>
+        <h1 className='text-2xl ml-2'>B-Lunch</h1>
+      </Link>
+      <div className='flex items-center'>
+        <nav>
+          <ul className='flex gap-2'>
+            <li className='cursor-pointer hover:opacity-70'>Products</li>
+          </ul>
+        </nav>
+        <BsCart4 className='text-2xl mx-5' />
+        <button className='bg-orange-600 px-3 py-1 rounded text-white'>Login</button>
+      </div>
+    </header>
   );
 }
