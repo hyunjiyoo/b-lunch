7d41ea63134b4d8ec79f17c173ee482494ab4c61
hyunjiyoo <hjyoo.dev@gmail.com>
2023-02-04 15:20:44 +0900

    Add tailwind plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,9 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
-        '23%': '23%',
+        '100px': '100px',
         '150px': '150px',
+        '180px': '180px',
         '250px': '250px',
         '350px': '350px',
       },
