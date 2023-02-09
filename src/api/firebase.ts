1e4c709e345fc0c2ef322b177dd2c3899bd71d97
hyunjiyoo <hjyoo.dev@gmail.com>
2023-02-09 17:30:41 +0900

    seperate firebase config value

--- src/api/firebase.ts
+++ src/api/firebase.ts
@@ -1,23 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: 'b-lunch-43bee.firebaseapp.com',
-  databaseURL: 'https://b-lunch-43bee-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'b-lunch-43bee',
-  storageBucket: 'b-lunch-43bee.appspot.com',
-  messagingSenderId: '1071261150474',
-  appId: '1:1071261150474:web:415ca518d9a80058a6def3',
-  measurementId: 'G-Y2EY2YWZ56',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID
 };
 
-// Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
 const auth = getAuth(firebase);
 const database = getDatabase(firebase);
