524127e39580db6990e1640b061c9d9ae2207c48
hyunjiyoo <hjyoo.dev@gmail.com>
2023-02-12 18:19:44 +0900

    style: select label

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -61,9 +61,9 @@ export default function Detail() {
   };
 
   return (
-    <div>
+    <>
       <p>카테고리 🔸 {category}</p>
-      <div className='grid lg:grid-cols-2 md:grid-cols-1 lg:gap-6'>
+      <section className='grid lg:grid-cols-2 md:grid-cols-1 lg:gap-6'>
         <div className='w-full h-350px mt-4 bg-orange-50 py-2'>
           <img src={imageUrl} alt={`${name}_image`} className='h-full object-full m-auto rounded' />
         </div>
@@ -73,8 +73,8 @@ export default function Detail() {
           <hr className='my-4' />
           <p className='text-sm text-zinc-500 h-100px'>{description}</p>
           <div className='flex items-center'>
-            <p className='mr-2 text-sm text-white py-1 px-2 border border-orange-500 bg-orange-500 font-semibold'>옵션</p>
-            <select value={selectedOption} className='my-4 border-2 border-orange-500 flex-1 p-1 text-slate-600 text-sm' onChange={selectOption}>
+            <label htmlFor='select' className='mr-2 text-sm text-white py-1 px-2 border border-orange-500 bg-orange-500 font-semibold'>옵션</label>
+            <select id='select' value={selectedOption} className='my-4 border-2 border-orange-500 flex-1 p-1 text-slate-600 text-sm outline-none' onChange={selectOption}>
               <option value=''>옵션을 선택해주세요</option>
               {option.split(',').map((opt: string, idx: number) => (
                 <option key={`${opt}_${idx}`} value={opt.trim()}>
@@ -87,7 +87,7 @@ export default function Detail() {
             장바구니에 담기
           </button>
         </div>
-      </div>
-    </div>
+      </section>
+    </>
   );
 }
