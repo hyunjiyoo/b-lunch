6a04758b6bd617b7e4a72db77b72da5f38332fed
hyunjiyoo <hjyoo.dev@gmail.com>
2023-03-29 17:34:31 +0900

    static test code about Product component

---
+++ src/components/Product/test/Product.test.tsx
@@ -0,0 +1,30 @@
+import { screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { ProductType } from 'types';
+import Product from '../Product';
+import { convertPriceFormat } from 'util/common';
+import { render } from 'util/test-utils';
+
+describe('Product', () => {
+  const product: ProductType = {
+    id: '1',
+    name: 'Test Product',
+    price: 1000,
+    category: 'Test Category',
+    description: 'Test description',
+    option: 'Test Option',
+    imageUrl: 'http://example.com/test.jpg',
+  };
+
+  it('renders product item', () => {
+    render(<Product product={product} />);
+
+    const image = screen.getByRole('img') as HTMLImageElement;
+    expect(image.alt).toBe(`${product.name}_image`);
+    expect(image.src).toBe(product.imageUrl);
+
+    expect(screen.getByText(product.name)).toBeInTheDocument();
+    expect(screen.getByText(convertPriceFormat(product.price))).toBeInTheDocument();
+    expect(screen.getByText(product.category)).toBeInTheDocument();
+  });
+});
