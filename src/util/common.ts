3c7f8db6cdb052e4a7072f851fd56bc985b2c794
hyunjiyoo <hjyoo.dev@gmail.com>
2023-06-21 21:30:26 +0900

    Update common.ts

--- src/util/common.ts
+++ src/util/common.ts
@@ -1,18 +1,18 @@
-import { initialUser } from "config/const";
+import { initialUser } from 'config/const';
 
 const convertPriceFormat = (price: number, won = '', unit = '₩') => {
   return unit + (+price).toLocaleString('en') + won;
-}
+};
 
 const getUserInfo = () => {
   const localStorageUser = localStorage.getItem('user');
   const user = localStorageUser ? JSON.parse(localStorageUser) : initialUser;
 
   return user || initialUser;
-}
+};
 
 const getCartItems = () => {
   return JSON.parse(localStorage.cart ?? JSON.stringify({}));
-}
+};
 
 export { convertPriceFormat, getUserInfo, getCartItems };
